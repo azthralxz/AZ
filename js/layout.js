82b60b131b87b876e53971b65643b16c5dac7939
azthralxz <azthralxz@gmail.com>
2026-02-01 23:43:52 +0700

    Delete js/layout.js

--- js/layout.js
+++
@@ -1,8 +0,0 @@
-async function loadComponent(id, file){
- const res = await fetch(file);
- const html = await res.text();
- document.getElementById(id).innerHTML = html;
-}
-
-loadComponent("navbar","components/navbar.html");
-loadComponent("loader","components/loader.html");
