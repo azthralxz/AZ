f9b490b4ce6e9cf18c0fc15de83fc6eb3056ac9f
azthralxz <azthralxz@gmail.com>
2026-02-01 22:34:47 +0700

    Add async function to load HTML components

---
+++ layout.js
@@ -0,0 +1,8 @@
+async function loadComponent(id, file){
+ const res = await fetch(file);
+ const html = await res.text();
+ document.getElementById(id).innerHTML = html;
+}
+
+loadComponent("navbar","components/navbar.html");
+loadComponent("loader","components/loader.html");
